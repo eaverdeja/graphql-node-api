eafe46feaa2a1a2b0b589886f35a3171fd4a50fc
Eduardo Verdeja <eaverdeja@hotmail.com>
2018-01-27 15:25:43 -0200

    JWT - Protegendo resolvers utilizando a composição de resolvers

---
+++ src/graphql/composable/composable.resolver.ts
@@ -0,0 +1,29 @@
+import { GraphQLFieldResolver } from "graphql"
+
+export type ComposableResolver<TSource, TContext> =
+    (fn: GraphQLFieldResolver<TSource, TContext>) => GraphQLFieldResolver<TSource, TContext>
+
+export function compose<TSource, TContext>(
+    ...funcs: Array<ComposableResolver<TSource, TContext>>
+): ComposableResolver<TSource, TContext> {
+    console.log(funcs[0])
+    if(funcs.length === 0) {
+        //if no functions, return the identity
+        return o => o
+    }
+
+    if(funcs.length === 1) {
+        return funcs[0]
+    }
+
+    const last = funcs[funcs.length - 1]
+    return (f: GraphQLFieldResolver<TSource, TContext>): GraphQLFieldResolver<TSource, TContext> => {
+        let result = last(f)
+        for(let index = funcs.length - 2; index >= 0; index--) {
+            const fn = funcs[index]
+            result = fn(result)
+            console.log(fn, result)
+        }
+        return result
+    }
+}
--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -3,6 +3,9 @@ import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { UserInstance } from "../../../models/UserModel";
 import { Transaction } from "sequelize";
 import { handleError } from "../../../utils/utils";
+import { compose } from "../../composable/composable.resolver";
+import { authResolver } from "../../composable/auth.resolver";
+import { verifyTokenResolver } from "../../composable/verify-token.resolver";
 
 export const userResolvers = {
     User: {
@@ -18,14 +21,14 @@ export const userResolvers = {
     },
 
     Query: {
-        users: (parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+        users: compose(authResolver, verifyTokenResolver)((parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
             return db.User
                 .findAll({
                     limit: first,
                     offset: offset
                 })
                 .catch(handleError)
-        },
+        }),
 
         user: (parent, {id}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
             id = parseInt(id)
